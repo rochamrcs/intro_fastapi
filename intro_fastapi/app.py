82869776d4697e7d4aa82f237c8033a478d2b3ef
rochamrcs <mavinicius95@gmail.com>
2025-05-11 21:33:46 -0300

    Conclusão aula de Introdução ao desenvolvimento WEB

--- intro_fastapi/app.py
+++ intro_fastapi/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from intro_fastapi.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/oimundo', status_code=HTTPStatus.OK)
+def ola_mundo():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,15 @@ from intro_fastapi.app import app
 def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)  # Arrange
 
-    response = client.get('/')  # Act
+    response = client.get('/olamundo')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+    assert response.json() == {'message': """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""}  # Assert

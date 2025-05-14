229ae3e065371a889579de0eab3f7e71f97998eb
rochamrcs <mavinicius95@gmail.com>
2025-05-14 19:54:44 -0300

    Inicio da Aula 3

--- intro_fastapi/app.py
+++ intro_fastapi/app.py
@@ -11,15 +11,3 @@ app = FastAPI()
 def read_root():
     return {'message': 'Olá Mundo!'}
 
-
-@app.get('/oimundo', status_code=HTTPStatus.OK)
-def ola_mundo():
-    return """
-    <html>
-      <head>
-        <title> Nosso olá mundo!</title>
-      </head>
-      <body>
-        <h1> Olá Mundo </h1>
-      </body>
-    </html>"""

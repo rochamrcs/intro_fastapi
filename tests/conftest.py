cd5e4dad136f4b4c0e633f621f45e4dcb3d9701e
rochamrcs <mavinicius95@gmail.com>
2025-05-18 17:44:49 -0300

    Aula 4 - Configurando o banco de dados e gerenciando migrações com Alembic

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,43 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from intro_fastapi.app import app
+from intro_fastapi.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
+
+    def fake_time_hook(mapper, connection, target):
+
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
